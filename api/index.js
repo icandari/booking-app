c7d8a902d013a7f9746a21c38c5ddfcc72dd6a27
icandari <isaaccandari@gmail.com>
2024-05-20 17:41:48 -1000

    Finish Hotel API

--- api/index.js
+++ api/index.js
@@ -29,10 +29,24 @@ mongoose.connection.on("disconnected", ()=> {
 
 //middlewares
 app.use(express.json())
+
 app.use("/api/auth", authRoute)
 app.use("/api/users", usersRoute)
 app.use("/api/hotels", hotelsRoute)
 app.use("/api/rooms", roomsRoute)
+
+
+//this section below will send a message if something goes wrong during the API request
+app.use((err,req,res,next) => {
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "Something went wrong!"
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 
 
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,41 +1,23 @@
 import express from "express";
 import Hotel from "../models/Hotel.js"
+import { createError } from "../utils/error.js";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req,res)=>{
-    
-    const newHotel = new Hotel(req.body)
-    
-    try {
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    } catch (error) {
-        res.status(500).json(err)
-    }
-});
-//UPDATE
-router.put("/:id", async (req,res)=>{
-    
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body}, {new: true}) //new true returns the updated data
-        res.status(200).json(updatedHotel)
-    } catch (error) {
-        res.status(500).json(err)
-    }
-});
-//DELETE   UPDATE THIS Yotuube video stopped at 30:19
-router.put("/:id", async (req,res)=>{
-    
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body}, {new: true}) //new true returns the updated data
-        res.status(200).json(updatedHotel)
-    } catch (error) {
-        res.status(500).json(err)
-    }
-});
-//GET
-//GET ALL
+router.post("/", createHotel);
 
-export default router
+//UPDATE
+router.put("/:id", updateHotel);
+
+//DELETE   
+router.delete("/:id", deleteHotel);
+
+//GET
+router.get("/:id", getHotel);
+
+//GET ALL
+router.get("/", getHotels);
+
+export default router;
